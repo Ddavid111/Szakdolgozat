91696f38563c9441f4649a3e707c2a3b71c2b7b9
Ddávid <90710356+Ddavid111@users.noreply.github.com>
2024-04-23 09:35:18 +0200

    Delete backend directory

--- backend/src/main/java/com/example/proba/dao/TopicDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.proba.dao;
-
-import com.example.proba.entity.Topic;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicDao extends CrudRepository<Topic, Integer> {
-}
